3319b5b426adcb59629cdeb169b76c09390e592a
kmaheshbabu75 <maheshbabu75k7@gmail.com>
2026-06-18 23:47:16 +0530

    Initial commit

---
+++ src/basics/EvenOrOdd.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class EvenOrOdd {
+    static void main(String[] args) {
+        int num = 234;
+        if ((num % 2) == 0) {
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+}
