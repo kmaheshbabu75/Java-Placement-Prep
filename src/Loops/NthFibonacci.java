2e73f47807c2a8208082a1c92829282bc9b0002d
kmaheshbabu75 <maheshbabu75k7@gmail.com>
2026-06-28 23:51:25 +0530

    first commit

---
+++ src/Loops/NthFibonacci.java
@@ -0,0 +1,27 @@
+package Loops;
+
+public class NthFibonacci {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        int previous1 = 0;
+        int previous2 = 1;
+        int current = 0;
+
+        for (int i = 2; i <= n; i++) {
+            current = previous1 + previous2;
+            previous1 = previous2;
+            previous2 = current;
+        }
+
+        if (n == 0) {
+            System.out.println(previous1);
+        } else if (n == 1) {
+            System.out.println(previous2);
+        } else {
+            System.out.println(current);
+        }
+    }
+}
